5573d011a6a7441a570fac44dc42962fdddd0b56
Jan Gimenez <jgimenez@nakima.es>
2020-01-02 16:25:04 +0100

    done

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+
+  currentUrl: string;
+
+  // constructor(private router: Router) {
+  //   this.router.events.subscribe((_: NavigationEnd) => this.currentUrl = _.url);
+  // }
+
+  constructor(private router: Router) {    
+    this.router.events.subscribe(e => {
+      if (e instanceof NavigationEnd) {
+        this.currentUrl = e.url;
+        console.log(this.currentUrl);
+        
+      }
+    });
+  }
+
+  ngOnInit() {   
+  }
+
+}
